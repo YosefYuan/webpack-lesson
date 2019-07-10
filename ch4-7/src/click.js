94348ff37fa5b21ab3adbfaddfadbcca91977b39
Yosef Yuan <yuanruixiangjet@163.com>
2019-07-11 00:10:59 +0800

    code split added

---
+++ ch4-7/src/click.js
@@ -0,0 +1,7 @@
+function handleClick() {
+    var element = document.createElement('div');
+    element.innerHTML = 'Yosef Yuan';
+    document.body.appendChild(element);
+}
+
+export default handleClick;
