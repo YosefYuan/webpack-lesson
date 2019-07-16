2880fadf3478e29089bbc95ec5885aa52e3e1102
Yosef Yuan <yuanruixiangjet@163.com>
2019-07-16 22:32:16 +0800

    env config

---
+++ ch4-12/build/webpack.common.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+module.exports = {
+    entry: {
+        main: './src/index.js',
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [{
+                    loader: 'babel-loader'
+                },
+                {
+                    loader: 'imports-loader?this=>window'
+                }
+            ]
+        }, {
+            test: /\.(jpg|png|gif|jpeg)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    name: '[name]_[hash].[ext]',
+                    outputPath: 'images/',
+                    limit: 8192
+                }
+            }
+        }, {
+            test: /\.(eot|ttf|svg|woff)$/,
+            use: {
+                loader: 'file-loader'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            '_join': ['lodash', 'join'],
+        })
+    ],
+    optimization: {
+        runtimeChunk: {
+            name: 'runtime'
+        },
+        usedExports: true,
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                    name: 'vendors'
+                }
+            }
+        },
+    },
+    performance: false,
+    output: {
+        path: path.resolve(__dirname, '../dist')
+    }
+}
