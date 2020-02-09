c426a443f28a2de4010cfe80da0224c1d4992924
Yosef Yuan <yuanruixiangjet@163.com>
2020-02-09 19:22:49 +0800

    feat: plugins: html-webpack-plugin clean-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-import avatar from './WechatIMG832.jpeg';
-import style from './index.scss';
-import createAvatar from './createAvatar';
-
-createAvatar();
-const img = new Image();
-img.src = avatar;
-img.classList.add(style.avatar);
-const root = document.getElementById('root');
-root.appendChild(img);
-
-console.log('avatar', avatar);
+var root = document.getElementById('root');
+root.innerHTML = '<div class="test">text</div>';
